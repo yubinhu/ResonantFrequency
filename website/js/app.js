feaa9043ffd6a16f68e809bcf109eff1576e1a05
Saksham Agarwal <sakshamagarwal@berkeley.edu>
2021-10-23 22:51:41 -0700

    redacted key

--- website/js/app.js
+++ website/js/app.js
@@ -1,15 +1,1 @@
-function weather(latitude, longitude){
-  var api_key = "173d82a42cf1523ad7e5376688d9921e";
-  return "api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&appid=" + api_key;
-}
-function location() {
-    if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-          var latitude = position.coords.latitude;
-          var longitude = position.coords.longitude;
-          return latitude, longitude;
-        });
-    }
-}
-var latitude, longitude = location();
-document.getElementById("result").innerHTML = weather(latitude, longitude);
+
